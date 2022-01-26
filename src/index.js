f000776e9fd8c59019e5262c40576aeee8c79eba
Best-Verie <irabestverie2016@gmail.com>
2022-01-26 12:22:48 +0200

    books mini components project

--- src/index.js
+++ src/index.js
@@ -6,11 +6,39 @@ import  ReactDOM  from 'react-dom'
 // }
 
 //using arrow function
-const Greeting = () =>{
-  return React.createElement('h1', {}, 'this is my page')
-}
+// const Greeting = () =>{
+//   return React.createElement('h1', {}, 'this is my page')
+// }
 
 //jsx rules
 
 
-ReactDOM.render(<Greeting/>, document.getElementById('root'))
+function BookList(){
+  return (
+  <section>
+    <Book/>
+    <Book/>
+    <Book/>
+  </section>)
+}
+
+const Book = () =>{
+  return <article>
+  <Image/>
+  <Title/>
+  <Author/>
+  </article>
+}
+
+const Image = () => (
+  <img src="https://images-na.ssl-images-amazon.com/images/I/81s0B6NYXML._AC_UL127_SR127,127_.jpg" alt=''></img>
+)
+
+const Title = () => (
+  <h1>It ends with us</h1>
+)
+
+const Author = () => (
+  <h4>Verie best</h4>
+)
+ReactDOM.render(<BookList/>, document.getElementById('root'))
